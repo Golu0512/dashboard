844b1fceb39a069c41e6c47f8f58b3b420692b80
Golu0512 <asif.hell111@gmail.com>
2023-06-06 12:40:04 +0530

    first commit of my app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import './App.css';
 import Header from "./components/Header";
-import SideBar from "./components/SideBar";
 function App() {
   return (
     <>
 
-      <div className='mb-2'>
-        <Header />
-      </div>
-
-      <div>
-        <SideBar />
-      </div>
+      <Header />
 
     </>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,49 @@
-import React from 'react'
-import { FaMoon, FaBell, FaUser } from "react-icons/fa";
+import React from "react";
 
 function Header() {
     return (
         <>
-            <div className='headerBar border d-flex justify-content-between align-items-center px-5'>
-
-                <p className='leftTitle my-0'>advance dashboard</p>
-
-                <div className='rightSide'>
-                    <ul className='headerList my-auto'>
-                        <li className='neonText'><FaMoon /></li>
-                        <li className='neonText'><FaBell /></li>
-                        <li className='neonText'><FaUser /></li>
-                    </ul>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+                Navbar
+            </a>
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                <a className="nav-link active" aria-current="page" href="#">
+                    Home
+                </a>
+                <a className="nav-link" href="#">
+                    Features
+                </a>
+                <a className="nav-link" href="#">
+                    Pricing
+                </a>
+                <a
+                    className="nav-link disabled"
+                    href="#"
+                    tabindex="-1"
+                    aria-disabled="true"
+                >
+                    Disabled
+                </a>
                 </div>
             </div>
+            </div>
+        </nav>
         </>
-    )
+    );
 }
 
-export default Header
+export default Header;
