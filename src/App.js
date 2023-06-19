5eb48013530ed215037557c954c3f9a2dd7894fd
Golu0512 <asif.hell111@gmail.com>
2023-06-20 00:10:46 +0530

    routes fixed

--- src/App.js
+++ src/App.js
@@ -1,45 +1,64 @@
 import React, { useState } from "react";
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
-// import ProductPage from "./components/ProductPage";
+import ProductPage from "./components/ProductPage";
 import Footer from "./components/Footer";
 function App() {
+  const products = {
+    id: "001",
+    pimg: "./images/med1.jpg",
+    title: "chemlopro",
+    combination:
+      "Researchers carry out investigations to find out which diseases are present, why they occur, what can treat or prevent them",
+    discription:
+      "Researchers carry out investigations to find out which diseases are present, why they occur, what can treat or prevent them, what makes them more likely to happen, and many other aspects of health. Clinical trials are one aspect of clinical research. They aim to find out if a therapy — often a drug — is safe and effective to use when treating a specific condition. The most effective way to demonstrate the effectiveness of a drug or technique is to carry out a double-blind, random, long-term, large clinical human study. In this type of study, researchers compare the effect of a therapy or drug in with either a placebo, no treatment, or another therapy or drug.",
+    indication:
+      "Researchers carry out investigations to find out which diseases are present, why they occur, what can treat or prevent them",
+    mrp: "120",
+  };
 
-  // const products = {
-  //   'id': '001',
-  //   'pimg': './images/med1.jpg',
-  //   'title': 'chemlopro',
-  //   'combination': 'Researchers carry out investigations to find out which diseases are present, why they occur, what can treat or prevent them',
-  //   'discription': 'Researchers carry out investigations to find out which diseases are present, why they occur, what can treat or prevent them, what makes them more likely to happen, and many other aspects of health. Clinical trials are one aspect of clinical research. They aim to find out if a therapy — often a drug — is safe and effective to use when treating a specific condition. The most effective way to demonstrate the effectiveness of a drug or technique is to carry out a double-blind, random, long-term, large clinical human study. In this type of study, researchers compare the effect of a therapy or drug in with either a placebo, no treatment, or another therapy or drug.',
-  //   'indication': 'Researchers carry out investigations to find out which diseases are present, why they occur, what can treat or prevent them',
-  //   'mrp': '120'
-  // };
+  const [mode, setMode] = useState("light");
 
-    const [mode, setMode] = useState('light');
+  const toggleMode = () => {
+    if (mode === "dark") {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+    } else {
+      setMode("dark");
+      document.body.style.backgroundColor = "black";
+      document.body.style.color = "white";
+    }
+  };
 
-    const toggleMode = () => {
-      if (mode === 'dark') {
-        setMode('light');
-        document.body.style.backgroundColor = 'white';
-        document.body.style.color = 'black';
-      } else {
-        setMode('dark');
-        document.body.style.backgroundColor = 'black';
-        document.body.style.color = 'white';
-      }
-    };
-
-    return (
-      <>
-
-        <Header toggleMode={toggleMode} mode={mode} />
-        <Home mode={mode} />
-        {/* <ProductPage id={products.id} title={products.title} combination={products.combination} discription={products.discription} indication={products.indication} mrp={products.mrp} pimg={products.pimg} /> */}
-        <Footer />
-
-      </>
-    );
+  return (
+    <>
+      <Header toggleMode={toggleMode} mode={mode} />
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Home mode={mode} />} />
+            <Route
+              exact
+              path="/productpage"
+              element={
+                <ProductPage
+                  id={products.id}
+                  title={products.title}
+                  combination={products.combination}
+                  discription={products.discription}
+                  indication={products.indication}
+                  mrp={products.mrp}
+                  pimg={products.pimg}
+                />
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 import React from "react";
 import Carousel from "./Carousel";
+import Homedata from "../Homedata.json";
+import { useNavigate } from "react-router-dom";
 
 function Home(props) {
+
+    const navigate = useNavigate();
+
+    const gotoProductPage = () =>{
+        navigate('./ProductPage')
+    }
+
     return (
-        <>
+        <> 
             <div className="my-2">
                 <Carousel />
             </div>
@@ -14,106 +23,29 @@ function Home(props) {
 
             {/* products section */}
 
-                <div className="row mt-5 px-0 mx-0">
-                    <div className="col-lg-4 col-md-4 col-sm-1 d-flex justify-content-center align-items-center mb-3">
-                        <div className={`card cardStyle bg-${props.mode} text-${props.mode === 'light' ? 'black' : 'white'}`}>
-                            <img className="card-img-top cardImage" src="./images/med1.jpg" height={'150'} alt="Card image cap" />
-                            <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">
-                                Some quick example text to build on the card title and make up
-                                the bulk of the card's content.
-                            </p>
-                            <a href="#" className="btn btn-primary">
-                                Go somewhere
-                            </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-lg-4 col-md-4 col-sm-1 d-flex justify-content-center align-items-center mb-3">
-                        <div className={`card cardStyle bg-${props.mode} text-${props.mode === 'light' ? 'black' : 'white'}`}>
-                            <img className="card-img-top cardImage" src="./images/med1.jpg" height={'150'} alt="Card image cap" />
-                            <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">
-                                Some quick example text to build on the card title and make up
-                                the bulk of the card's content.
-                            </p>
-                            <a href="#" className="btn btn-primary">
-                                Go somewhere
-                            </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-lg-4 col-md-4 col-sm-1 d-flex justify-content-center align-items-center mb-3">
-                        <div className={`card cardStyle bg-${props.mode} text-${props.mode === 'light' ? 'black' : 'white'}`}>
-                            <img className="card-img-top cardImage" src="./images/med1.jpg" height={'150'} alt="Card image cap" />
-                            <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">
-                                Some quick example text to build on the card title and make up
-                                the bulk of the card's content.
-                            </p>
-                            <a href="#" className="btn btn-primary">
-                                Go somewhere
-                            </a>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div className="row px-0 mx-0">
-                    <div className="col-lg-4 col-md-4 col-sm-1 d-flex justify-content-center align-items-center mb-3">
-                        <div className={`card cardStyle bg-${props.mode} text-${props.mode === 'light' ? 'black' : 'white'}`}>
-                            <img className="card-img-top cardImage" src="./images/med1.jpg" height={'150'} alt="Card image cap" />
-                            <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">
-                                Some quick example text to build on the card title and make up
-                                the bulk of the card's content.
-                            </p>
-                            <a href="#" className="btn btn-primary">
-                                Go somewhere
-                            </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-lg-4 col-md-4 col-sm-1 d-flex justify-content-center align-items-center mb-3">
-                        <div className={`card cardStyle bg-${props.mode} text-${props.mode === 'light' ? 'black' : 'white'}`}>
-                            <img className="card-img-top cardImage" src="./images/med1.jpg" height={'150'} alt="Card image cap" />
-                            <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">
-                                Some quick example text to build on the card title and make up
-                                the bulk of the card's content.
-                            </p>
-                            <a href="#" className="btn btn-primary">
-                                Go somewhere
-                            </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-lg-4 col-md-4 col-sm-1 d-flex justify-content-center align-items-center mb-3">
-                        <div className={`card cardStyle bg-${props.mode} text-${props.mode === 'light' ? 'black' : 'white'}`}>
-                            <img className="card-img-top cardImage" src="./images/med1.jpg" height={'150'} alt="Card image cap" />
-                            <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">
-                                Some quick example text to build on the card title and make up
-                                the bulk of the card's content.
-                            </p>
-                            <a href="#" className="btn btn-primary">
-                                Go somewhere
-                            </a>
-                            </div>
-                        </div>
-                    </div>
-
+                <div className="row mt-5 px-0 mx-0 d-flex justify-content-center align-items-center">
+                    
+                        {
+                            Homedata.map(value => {
+                                return(
+                                    <div className="col-lg-4 col-md-4 col-10 d-flex justify-content-center align-items-center mb-4" key={value.id}>
+                                        <div className={`card cardStyle bg-${props.mode} text-${props.mode === 'light' ? 'black' : 'white'}`}>
+                                            <img className="card-img-top cardImage" src={value.pimg} height={'150'} alt={value.name} />
+                                            <div className="card-body">
+                                            <h5 className="card-title">{value.name}</h5>
+                                            <p className="card-text">
+                                                Some quick example text to build on the card title and make up
+                                                the bulk of the card's content.
+                                            </p>
+                                            <button className="btn btn-primary" onClick={gotoProductPage}>
+                                                View
+                                            </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                );
+                            })
+                        }
                 </div>
 
         </>
