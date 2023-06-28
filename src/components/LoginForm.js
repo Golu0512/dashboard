3465bf319baa6d21a8826e2addff4c635e655003
Golu0512 <asif.hell111@gmail.com>
2023-06-28 21:20:47 +0530

    fixed

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -8,7 +8,7 @@ function LoginForm() {
         password: ''
     });
 
-        const getLocalStorageData = () =>{
+    const getLocalStorageData = () =>{
         let localStorageData = localStorage.getItem('userLogin');
         if (localStorageData) {
             return JSON.parse(localStorage.getItem('userLogin'));
@@ -42,39 +42,39 @@ function LoginForm() {
             <pre>{JSON.stringify(userLogin)}</pre>
             <div className="bg-info d-flex justify-content-center align-items-center py-3">
                 <form onSubmit={handleSubmit} className="w-25">
-                <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">
-                    Email address
-                    </label>
-                    <input
-                    type="email"
-                    value={userLogin.email}
-                    onChange={getLogin}
-                    name="email"
-                    className="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                    />
-                    <div id="emailHelp" className="form-text">
-                    We'll never share your email with anyone else.
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputEmail1" className="form-label">
+                            Email address
+                        </label>
+                        <input
+                            type="email"
+                            value={userLogin.email}
+                            onChange={getLogin}
+                            name="email"
+                            className="form-control"
+                            id="exampleInputEmail1"
+                            aria-describedby="emailHelp"
+                        />
+                        <div id="emailHelp" className="form-text">
+                            We'll never share your email with anyone else.
+                        </div>
                     </div>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">
-                    Password
-                    </label>
-                    <input
-                    type="password"
-                    value={userLogin.password}
-                    onChange={getLogin}
-                    name="password"
-                    className="form-control"
-                    id="exampleInputPassword1"
-                    />
-                </div>
-                <button type="submit" className="btn btn-primary">
-                    Submit
-                </button>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputPassword1" className="form-label">
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            value={userLogin.password}
+                            onChange={getLogin}
+                            name="password"
+                            className="form-control"
+                            id="exampleInputPassword1"
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-primary">
+                        Submit
+                    </button>
                 </form>
             </div>
         </>
